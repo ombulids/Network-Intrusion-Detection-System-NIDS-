330d64dcf4f1b9689862c2a3819c7829f6fcba08
ombulids <ompakhale8996@gmail.com>
2025-11-25 13:46:31 +0530

    Processing Data Code

--- project.py
+++ project.py
@@ -3,8 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-
-# 1. Define the column names (The NSL-KDD dataset has 43 columns)
+import joblib
 columns = [
     'duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes',
     'land', 'wrong_fragment', 'urgent', 'hot', 'num_failed_logins',
@@ -18,27 +17,22 @@ columns = [
     'dst_host_serror_rate', 'dst_host_srv_serror_rate', 'dst_host_rerror_rate',
     'dst_host_srv_rerror_rate', 'attack', 'level'
 ]
-
-# 2. Load the dataset
-# header=None tells pandas "There is no header in the file"
-# names=columns tells pandas "Use these specific names for the columns"
 df = pd.read_csv("KDDTrain+.txt", header=None, names=columns)
-
-# 3. Check the structure
 print("Shape of dataset:", df.shape)
+df['attack'] = df['attack'].str.strip()
+df['attack'] = df['attack'].apply(lambda x: 0 if x == 'normal' else 1)
 label_encoder = LabelEncoder()
 print(label_encoder)
-df['attack'] = label_encoder.fit_transform(df['attack'])
 df['protocol_type'] = label_encoder.fit_transform(df['protocol_type'])
 df['service'] = label_encoder.fit_transform(df['service'])
 df['flag'] = label_encoder.fit_transform(df['flag'])
 X = df.iloc[:,0:41]
 y = df['attack']
-print(y)
 Xtrain , Xtest, ytrain , ytest = train_test_split(X,y,random_state=11,test_size=0.2)
 rf = RandomForestClassifier()
 rf.fit(Xtrain,ytrain)
 y_pred = rf.predict(Xtest)
 print(rf.score(Xtest,ytest))
 print(classification_report(ytest,y_pred))
-
+print(f"Prediction of First Package:{y_pred[0]}")
+joblib.dump(rf ,'Model(Nids)')
